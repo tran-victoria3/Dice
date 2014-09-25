4b509e9140cb698d93a21bfe66f240c4e33faf0c
unknown <vitran9@697-334-Gollum.sf.k12.internal>
2014-09-25 11:27:54 -0700

    finished program

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,11 +1,27 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 //Victoria Tran AP Computer Science Dice Program Block 4
 
-void setup()
+public void setup()
 {
 	size(500,500);
 	noLoop();
 }
-void draw()
+public void draw()
 {
 	int sum = 0;
 	for(int x =0; x <501; x+=8){
@@ -24,7 +40,7 @@ void draw()
 	stroke(0);
 }
 
-void mousePressed()
+public void mousePressed()
 {
 	
 	redraw();
@@ -43,12 +59,12 @@ class Die //models one single dice cube
 
 
 	}
-	void roll()
+	public void roll()
 	{
 		number = (int)(Math.random()*6 +1);
 	}
 
-	void show()
+	public void show()
 	{
 		
 		rect(xPosition,yPosition,10,10);
@@ -97,4 +113,13 @@ class Die //models one single dice cube
 
 
 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
